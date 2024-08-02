6ea7b60d8e3fec6929fca43994a88c66a11e6241
AlecCross <cross2472@gmail.com>
2024-08-02 06:42:45 -0800

    Add GTM

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,21 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html>
       <Head>
+        {/* Global Site Tag (gtag.js) - Google Tag Manager */}
+        <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-P54XXNBX');
+              `,
+            }}
+          />
         <title>Sticker Converter</title>
         <meta property="og:title" content="Sticker Converter" key="title" />
         <meta property="og:image" content="/icon-512x512.png" />
@@ -28,6 +40,16 @@ export default function Document() {
         <link rel="manifest" href="/manifest.json" />
       </Head>
       <body>
+        {/*Google Tag Manager (noscript)*/}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P54XXNBX"
+            height="0"
+            width="0"
+            style={{display: "none", visibility: "hidden",}}
+          ></iframe>
+        </noscript>
+        {/*End Google Tag Manager (noscript)*/}
         <Main />
         <NextScript />
       </body>
